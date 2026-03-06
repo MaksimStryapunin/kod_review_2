f4a2ecc5ccaa55c7be7798ab4439edd1d457e61e
UsrMakson <166782494+UsrMakson@users.noreply.github.com>
2026-03-06 13:48:14 +0500

    Добавьте файлы проекта.

---
+++ 02/Task04.cpp
@@ -0,0 +1,220 @@
+﻿/* Дан двусвязный линейный список и указатель на первый элемент этого 
+списка. Создать текстовый файл, в который вывести содержимое списка в обратном 
+порядке. По ходу вывода списка удалять элементы в нем. Числа выводить в одну строку, 
+разделяя ровно одним пробелом, в конце строки пробел на ставить.
+
+
+Все динамические структуры данных реализовывать через классы. Не использовать STL.  
+Для каждой динамической структуры должен быть предусмотрен стандартный
+набор методов - добавления/удаления/вывода элементов. Во всех задачах обязательно наличие 
+дружественного интерфейса. Ввод данных с клавиатуры. */
+
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <locale>
+using namespace std;
+
+// Структура узла двусвязного списка
+struct Uzel {
+    int Dannye;         // Данные узла
+    Uzel* Predydushiy;  // Указатель на предыдущий узел
+    Uzel* Sleduyushiy;  // Указатель на следующий узел
+};
+
+// Класс для работы с двусвязным списком
+class Spisok {
+private:
+    Uzel* Perviy; // Указатель на первый узел
+
+public:
+    // Инициализация списка
+    void Inicializacia() {
+        Perviy = nullptr;
+    }
+
+    // Добавление элемента в конец списка
+    void DobavitElement(int Znachenie) {
+        Uzel* Novyj = new Uzel;
+        Novyj->Dannye = Znachenie;
+        Novyj->Predydushiy = nullptr;
+        Novyj->Sleduyushiy = nullptr;
+
+        if (Perviy == nullptr) {
+            Perviy = Novyj;
+        }
+        else {
+            Uzel* Tekushiy = Perviy;
+            while (Tekushiy->Sleduyushiy != nullptr) {
+                Tekushiy = Tekushiy->Sleduyushiy;
+            }
+            Tekushiy->Sleduyushiy = Novyj;
+            Novyj->Predydushiy = Tekushiy;
+        }
+    }
+
+    // Удаление указанного узла
+    void UdalitElement(Uzel* UzelDlyaUdaleniya) {
+        if (UzelDlyaUdaleniya == nullptr) return;
+
+        Uzel* Pred = UzelDlyaUdaleniya->Predydushiy;
+        Uzel* Sled = UzelDlyaUdaleniya->Sleduyushiy;
+
+        if (Pred != nullptr) {
+            Pred->Sleduyushiy = Sled;
+        }
+        else {
+            Perviy = Sled;
+        }
+
+        if (Sled != nullptr) {
+            Sled->Predydushiy = Pred;
+        }
+
+        delete UzelDlyaUdaleniya;
+    }
+
+    // Вывод списка на экран
+    void Pechat() const {
+        Uzel* Tekushiy = Perviy;
+        cout << "Список: ";
+        while (Tekushiy != nullptr) {
+            cout << Tekushiy->Dannye << " ";
+            Tekushiy = Tekushiy->Sleduyushiy;
+        }
+        cout << endl;
+    }
+
+    // Запись в файл в обратном порядке и удаление элементов
+    void ZapisatVFailIOchistit(const string& ImyaFaila) {
+        ofstream file(ImyaFaila);
+        if (!file.is_open()) {
+            cout << "Ошибка открытия файла!" << endl;
+            return;
+        }
+
+        bool PerviyElement = true;
+        Uzel* Tekushiy = Perviy;
+
+        // Находим последний элемент
+        while (Tekushiy != nullptr && Tekushiy->Sleduyushiy != nullptr) {
+            Tekushiy = Tekushiy->Sleduyushiy;
+        }
+
+        // Записываем и удаляем элементы с конца
+        while (Tekushiy != nullptr) {
+            if (PerviyElement) {
+                file << Tekushiy->Dannye;
+                PerviyElement = false;
+            }
+            else {
+                file << " " << Tekushiy->Dannye;
+            }
+
+            Uzel* Predydushiy = Tekushiy->Predydushiy;
+            UdalitElement(Tekushiy);
+            Tekushiy = Predydushiy;
+        }
+
+        file.close();
+        cout << "Данные записаны в файл и список очищен." << endl;
+    }
+
+    // Очистка всего списка
+    void Ochistka() {
+        Uzel* Tekushiy = Perviy;
+        while (Tekushiy != nullptr) {
+            Uzel* Sleduyushiy = Tekushiy->Sleduyushiy;
+            delete Tekushiy;
+            Tekushiy = Sleduyushiy;
+        }
+        Perviy = nullptr;
+    }
+};
+
+// Функция для ввода числа с проверкой
+int VvodChisla() {
+    string Vvod;
+    while (true) {
+        cout << "Введите число (или '-1' для завершения): ";
+        cin >> Vvod;
+
+        if (Vvod == "-1") {
+            return -1;
+        }
+
+        bool Korrektno = true;
+        int Znak = 1;
+        size_t Start = 0;
+
+        if (Vvod.empty()) {
+            Korrektno = false;
+        }
+        else if (Vvod[0] == '-') {
+            Znak = -1;
+            Start = 1;
+            if (Vvod.size() == 1) Korrektno = false;
+        }
+
+        for (size_t i = Start; i < Vvod.size(); ++i) {
+            if (!isdigit(Vvod[i])) Korrektno = false;
+        }
+
+        if (Korrektno) {
+            int Chislo = 0;
+            for (size_t i = Start; i < Vvod.size(); ++i) {
+                Chislo = Chislo * 10 + (Vvod[i] - '0');
+            }
+            return Chislo * Znak;
+        }
+        else {
+            cout << "Некорректный ввод! Используйте цифры и знак '-' в начале." << endl;
+        }
+    }
+}
+
+// Новая функция для вывода содержимого файла
+void VivodSoderzhimogoFaila(const string& ImyaFaila) {
+    ifstream file(ImyaFaila);
+    if (!file.is_open()) {
+        cout << "Ошибка при открытии файла для чтения!" << endl;
+        return;
+    }
+
+    cout << "\n=== Содержимое файла ===" << endl;
+    string Stroka;
+    while (getline(file, Stroka)) {
+        cout << Stroka << endl;
+    }
+    file.close();
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Spisok MoiSpisok;
+    MoiSpisok.Inicializacia();
+
+    cout << "=== Ввод чисел в список ===" << endl;
+    while (true) {
+        int Chislo = VvodChisla();
+        if (Chislo == -1) break;
+        MoiSpisok.DobavitElement(Chislo);
+    }
+
+    MoiSpisok.Pechat();
+
+    cout << "\n=== Запись в файл ===" << endl;
+    string ImyaFaila;
+    cout << "Введите имя файла: ";
+    cin >> ImyaFaila;
+
+    MoiSpisok.ZapisatVFailIOchistit(ImyaFaila);
+    VivodSoderzhimogoFaila(ImyaFaila); // Вызов новой функции
+    MoiSpisok.Ochistka();
+
+    return 0;
+}
---
+++ 10/Task03.cpp
@@ -0,0 +1,146 @@
+﻿
+/*Дан односвязный линейный список и указатель на голову списка P1. Необходимо
+вставить значение M перед каждым вторым элементом списка, и вывести ссылку на последний
+элемент полученного списка P2. При нечетном числе элементов исходного списка в конец
+списка вставлять не надо.
+
+
+Для каждой динамической структуры должен быть предусмотрен стандартный набор методов - 
+добавления/удаления/вывода элементов. 
+Во всех задачах обязательно наличие дружественного интерфейса. Ввод данных с клавиатуры.
+
+*/
+
+
+
+
+#include <iostream>
+using namespace std;
+class Node {
+public:
+    int data;       
+    Node* next;     
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head; 
+
+public:
+    
+    LinkedList() : head(nullptr) {}
+
+    void add(int value) {
+        Node* newNode = new Node(value); 
+        if (head == nullptr) {           
+            head = newNode;              
+        }
+        else {
+            Node* current = head;        
+            while (current->next != nullptr) { 
+                current = current->next;
+            }
+            current->next = newNode;   
+        }
+    }
+
+    // Метод для вставки значения M перед каждым вторым элементом
+    void insertBeforeEverySecond(int M) {
+        Node* current = head;          
+        int count = 1;                  
+
+        while (current != nullptr && current->next != nullptr) {
+            if (count % 2 == 1) {      
+                Node* newNode = new Node(M); 
+                newNode->next = current->next; 
+                current->next = newNode; 
+                current = newNode->next; 
+            }
+            else {
+                current = current->next; 
+            }
+            count++;                  
+        }
+    }
+
+    
+    void print() {
+        Node* current = head;          
+        while (current != nullptr) {    
+            cout << current->data << " "; 
+            current = current->next;    
+        }
+        cout << endl;         
+    }
+
+    // Метод для получения указателя на последний элемент списка
+    Node* getLastNode() {
+        Node* current = head;           // Начинаем с головы списка
+        while (current != nullptr && current->next != nullptr) { // Идем до последнего узла
+            current = current->next;
+        }
+        return current;                 // Возвращаем указатель на последний узел
+    }
+    void clearList() {
+        while (head != nullptr) {       
+            Node* temp = head;          
+            head = head->next;         
+            delete temp;               
+        }
+        cout << "Список очищен." << endl;
+    }
+    
+    friend void insertBeforeEverySecond(LinkedList& list, int M);
+};
+
+
+void insertBeforeEverySecond(LinkedList& list, int M) {
+    list.insertBeforeEverySecond(M);
+}
+
+int main() {
+    LinkedList list;                     // Создаем список
+    int n, value, M;
+
+    
+    cout << "Введите количество элементов в списке: ";
+    cin >> n;
+    cout << "Введите элементы списка: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> value;               // Вводим значение элемента
+        list.add(value);                 // Добавляем элемент в список
+    }
+
+   
+    cout << "Введите значение M для вставки: ";
+    cin >> M;
+
+    // Вставка значения M перед каждым вторым элементом
+    insertBeforeEverySecond(list, M);
+
+    
+    cout << "Список после вставки: ";
+    list.print();
+
+    // Получение указателя на последний элемент списка
+    Node* lastNode = list.getLastNode();
+
+    // Вывод результата
+    if (lastNode != nullptr) {
+        cout << "Указатель на последний элемент списка: " << lastNode << endl;
+        cout << "Значение последнего элемента: " << lastNode->data <<endl;
+    }
+    else {
+        cout << "Список пуст." << endl;
+
+    }
+    list.clearList();
+
+    // Проверка, что список пуст
+    cout << "Проверка списка после очистки: ";
+    list.print();
+
+
+    return 0;                           
+}
